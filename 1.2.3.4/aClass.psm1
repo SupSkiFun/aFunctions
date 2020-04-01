cd0a084a1427d3b1a95cd61836fed84f9aa65085
Joe Acosta <aumjoey@gmail.com>
2020-03-31 21:21:38 -0700

    final

---
+++ 1.2.3.4/aClass.psm1
@@ -0,0 +1,23 @@
+class aClass
+{
+    static [PSCustomObject] MakeSEC2IObj ([psobject] $obj )
+    {
+        $lo = [pscustomobject]@{
+            Name = ($obj.Tags |
+                Where-Object {$_.Key -match "Name"}).Value
+            ID = $obj.InstanceId
+            PrivateIP = $obj.PrivateIpAddress
+            PublicIP = $obj.PublicIpAddress
+            PublicDNS = $obj.PublicDnsName
+            Type = $obj.InstanceType.Value
+            SecurityGroupName = $obj.SecurityGroups.GroupName
+            SecurityGroupID = $obj.SecurityGroups.GroupId
+            Tags = $obj.Tags
+            State = $obj.State.Name
+            SubnetID = $obj.SubnetId
+            VpcID = $obj.VpcId
+        }
+        $lo.PSObject.TypeNames.Insert(0,'SupSkiFun.AWS.EC2Instance.Info')
+        return $lo
+    }
+}
